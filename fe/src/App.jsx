b167a6e536927f9d2b7713190e6ea077add138cd
Minh <dangminhn10@gmail.com>
2025-07-11 16:25:28 +0700

    setup file and build page login

---
+++ fe/src/App.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login.jsx'
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ fe/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
